684fa42c356a389dde15446475dbf675bbc31221
Archik94 <krevetkooo@icloud.com>
2025-12-08 21:38:37 +0500

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -23,38 +23,61 @@ async def get_item(session: AsyncSession, orm_cls: ORM_CLS, item_id: int) -> ORM
     return orm_obj
 
 
+#async def get_item_by_qs(session: AsyncSession, title: str | None,
+#                         description: str | None, price: str | None, creator: str | None):
+#    sql_str = 'SELECT * from advertisement'
+#    flag = 0
+#    if title:
+#        sql_str += f" WHERE title = '{title}'"
+#        flag = 1
+#    if description:
+#        if flag == 0:
+#            sql_str += f" WHERE description LIKE '%{description}%'"
+#        else:
+#            sql_str += f" AND description LIKE '%{description}%'"
+#        flag = 1
+#    if price:
+#        price = float(price)
+#        if flag == 0:
+#            sql_str += f' WHERE price = {price}'
+#        else:
+#            sql_str += f' AND price = {price}'
+#        flag = 1
+#    if creator:
+#        if flag == 0:
+#            sql_str += f" WHERE creator = '{creator}'"
+#        else:
+#            sql_str += f" AND creator = '{creator}'"
+#    sql = text(sql_str)
+#    orm_objects = await session.execute(sql)
+#    orm_objects_dict = orm_objects.mappings().all()
+#    if not orm_objects_dict:
+#        raise HTTPException(status_code=404, detail="Item not found")
+#    return orm_objects_dict
 async def get_item_by_qs(session: AsyncSession, title: str | None,
-                         description: str | None, price: str | None, creator: str | None):
-    sql_str = 'SELECT * from advertisement'
-    flag = 0
+                         description: str | None, price: float | None, author: str | None):
+    query = select(models.Adv)
+    
+    filters = []
     if title:
-        sql_str += f" WHERE title = '{title}'"
-        flag = 1
+        filters.append(models.Adv.title == title)
     if description:
-        if flag == 0:
-            sql_str += f" WHERE description LIKE '%{description}%'"
-        else:
-            sql_str += f" AND description LIKE '%{description}%'"
-        flag = 1
+        filters.append(models.Adv.description.ilike(f"%{description}%"))
     if price:
-        price = float(price)
-        if flag == 0:
-            sql_str += f' WHERE price = {price}'
-        else:
-            sql_str += f' AND price = {price}'
-        flag = 1
-    if creator:
-        if flag == 0:
-            sql_str += f" WHERE creator = '{creator}'"
-        else:
-            sql_str += f" AND creator = '{creator}'"
-    sql = text(sql_str)
-    orm_objects = await session.execute(sql)
-    orm_objects_dict = orm_objects.mappings().all()
-    if not orm_objects_dict:
+        filters.append(models.Adv.price == price)
+    if author:
+        filters.append(models.Adv.author == author)
+    
+    if filters:
+        query = query.where(*filters)
+    
+    result = await session.execute(query)
+    advs = result.scalars().all()
+    
+    if not advs:
         raise HTTPException(status_code=404, detail="Item not found")
-    return orm_objects_dict
-
+    
+    return [adv.dict for adv in advs]
 
 async def delete_item(session: AsyncSession, orm_cls: ORM_CLS, item_id) -> None:
     orm_obj = await get_item(session, orm_cls, item_id)
