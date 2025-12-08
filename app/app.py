6ad5d421e30caff6f20834e4411f9a2e9fa69e44
Archik94 <krevetkooo@icloud.com>
2025-12-08 20:51:15 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -11,27 +11,27 @@ app = fastapi.FastAPI(
 )
 
 
-@app.get("/v1/advertisement/{advertisement_id}", response_model=schema.GetAdvResponse)
+@app.get("/advertisement/{advertisement_id}", response_model=schema.GetAdvResponse)
 async def get_adv(advertisement_id: int, session: SessionDependency):
     adv = await crud.get_item(session, models.Adv, advertisement_id)
     return adv.dict
 
 
-@app.get("/v1/advertisement")
-async def get_adv_by_qs(session: SessionDependency, title: str = None,
+@app.get("/advertisement")
+async def get_item_by_qs(session: SessionDependency, title: str = None,
                         description: str = None, price: str = None, creator: str = None):
     advs = await crud.get_item_by_qs(session, title, description, price, creator)
     return advs
 
 
-@app.post("/v1/advertisement", response_model=schema.CreateAdvResponse)
+@app.post("/advertisement", response_model=schema.CreateAdvResponse)
 async def create_adv(adv_json: schema.CreateAdvRequest, session: SessionDependency):
     adv = models.Adv(**adv_json.dict())
     adv = await crud.add_item(session, adv)
     return adv.id_dict
 
 
-@app.patch("/v1/advertisement/{advertisement_id}", response_model=schema.UpdateAdvResponse)
+@app.patch("/advertisement/{advertisement_id}", response_model=schema.UpdateAdvResponse)
 async def update_todo(
     advertisement_id: int, adv_json: schema.UpdateAdvRequest, session: SessionDependency
 ):
@@ -44,7 +44,7 @@ async def update_todo(
     return adv.id_dict
 
 
-@app.delete("/v1/advertisement/{advertisement_id}", response_model=schema.DeleteAdvResponse)
+@app.delete("/advertisement/{advertisement_id}", response_model=schema.DeleteAdvResponse)
 async def delete_adv(advertisement_id: int, session: SessionDependency):
     await crud.delete_item(session, models.Adv, advertisement_id)
     return STATUS_SUCCESS_RESPONSE
